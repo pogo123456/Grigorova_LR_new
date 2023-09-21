6f955f2502c81b48ae1391de187db991fedd489d
pogo123456 <grigorova-e-05@mail.ru>
2023-09-21 15:33:01 +0300

    правки проверки данных

--- Grigorova_as_04/Grigorova_as_04.cpp
+++ Grigorova_as_04/Grigorova_as_04.cpp
@@ -9,7 +9,7 @@ struct pipe
 {
 	string km_mark = "";
 	double len = 0;
-	double diam = 0;
+	int diam = 0;
 	bool inrepair = true;
 }; 
 
@@ -18,59 +18,57 @@ struct station
 	string title = "";
 	int workshop = 0;
 	int active_workshop = 0;
-	double efficiency = 'A';
+	char efficiency = 'A';
 };
 
-void CheckInput(int& var)
+void InputInt(int& var)
 {
 	cin >> var;
-	while (cin.fail() || var <= 0)
+	while (cin.fail() || cin.peek() != '\n' || var <= 0)
 	{
 		cin.clear();
 		cin.ignore(1000, '\n');
-		cout << "Try again";
+		cout << "Try again: ";
 		cin >> var;
 	}
 }
 
-void CheckInput(double& var)
+void InputDouble(double& var)
 {
 	cin >> var;
-	while (cin.fail() || var <= 0)
+	while (cin.fail() || cin.peek() != '\n' || var <= 0)
 	{
 		cin.clear();
 		cin.ignore(1000, '\n');
-		cout << "Try again";
+		cout << "Try again: ";
 		cin >> var;
 	}
 }
 
-void CheckInput(bool& var)
+void InputBool(bool& var)
 {
-	int input;
-	cin >> input;
-	while (cin.fail() || !(input == 0 || input == 1))
+	cin >> var;
+	while (cin.fail())
 	{
 		cin.clear();
 		cin.ignore(1000, '\n');
-		cout << "Try again";
-		cin >> input;
+		cout << "Try again: ";
+		cin >> var;
 	}
-	var = input == 1;
 }
 
 pipe AddPipe()
 {
 	pipe new_pipe;
-	cout << "Please, enter kilometer mark: " << endl;
+	cout << "Please, enter kilometer mark:  ";
 	cin.ignore();
 	getline(cin, new_pipe.km_mark);
-	cout << "Please, enter pipe length(km): " << endl;
-	CheckInput(new_pipe.len);
-	cout << "Please, enter pipe diameter(mm): " << endl;
-	CheckInput(new_pipe.diam);
-	cout << "Please, enter pipe inrepair? (1-yes, 0-no): " << endl;
-	CheckInput(new_pipe.inrepair);
+	cout << "Please, enter pipe length(km):  ";
+	InputDouble(new_pipe.len);
+	cout << "Please, enter pipe diameter(mm):  ";
+	InputInt(new_pipe.diam);
+	cout << "Please, enter pipe inrepair? (1-yes, 0-no):  ";
+	InputBool(new_pipe.inrepair);
 
 	return new_pipe;
 }
@@ -78,19 +76,19 @@ pipe AddPipe()
 station AddStation()
 {
 	station new_station;
-	cout << "Please, enter station name: " << endl;
+	cout << "Please, enter station name:  ";
 	cin.ignore();
 	getline(cin, new_station.title);
-	cout << "Please, enter number of workshops: " << endl;
-	CheckInput(new_station.workshop);
-	cout << "Please, enter number of active workshops: " << endl;
-	CheckInput(new_station.active_workshop);
+	cout << "Please, enter number of workshops:  ";
+	InputInt(new_station.workshop);
+	cout << "Please, enter number of active workshops:  ";
+	InputInt(new_station.active_workshop);
 	while (new_station.workshop < new_station.active_workshop)
 	{
 		cout << "Error! Quantity exceeded" << endl << "Enter correct data: " << endl;
-		cin >> new_station.active_workshop;
+		InputInt(new_station.active_workshop);
 	}
-	cout << "Please, enter efficiency from A to G: " << endl;
+	cout << "Please, enter efficiency from A to G:  ";
 	cin >> new_station.efficiency;
 	while (!(new_station.efficiency >= 'A' && new_station.efficiency <= 'G'))
 	{
@@ -110,7 +108,7 @@ void ShowPipe(const pipe& new_pipe)
 		cout << "Pipe length: " << new_pipe.len << endl;
 		cout << "Pipe diameter: " << new_pipe.diam << endl;
 		cout << "Kilometer mark: " << new_pipe.km_mark << endl;
-		if (new_pipe.inrepair)
+		if (new_pipe.inrepair)//?????????
 			cout << "Pipe under repair." << endl;
 		else
 			cout << "The pipe is working." << endl;
@@ -134,14 +132,14 @@ void EditPipe(pipe& edit_pipe)
 {
 	int choice = 0;
 	if (edit_pipe.len == 0)
-		cout << "" << endl;
+		cout << "No pipe" << endl;
 	else
 	{
-		if (edit_pipe.inrepair == true)
+		if (edit_pipe.inrepair)
 		{
 			cout << "Pipe under repair. Want to fix it?" << endl;
 			cout << "1. Yes" << endl << "2. No" << endl << "Your choice: ";
-			cin >> choice;
+			InputInt(choice);
 			switch (choice)
 			{
 			case 1:
@@ -160,7 +158,7 @@ void EditPipe(pipe& edit_pipe)
 		{
 			cout << "The pipe is working. Want to fix it?" << endl;
 			cout << "1. Yes" << endl << "2. No" << endl << "Your choice: ";
-			cin >> choice;
+			InputInt(choice);
 			switch (choice)
 			{
 			case 1:
@@ -187,17 +185,17 @@ void EditStation(station& edit_station)
 	{
 		cout << "Want to fix number?" << endl;
 		cout << "1. Yes" << endl << "2. No" << endl << "Your choice: ";
-		cin >> choice;
+		InputInt(choice);
 		switch (choice)
 		{
 		case 1:
 			cout << "Please, enter number of active workshops: ";
-			cin >> edit_station.active_workshop;
+			InputInt(edit_station.active_workshop);
 			while (edit_station.active_workshop > edit_station.workshop)
 			{
 				cout << "Erorr! Quantity exceeded." << endl << "Number of workshops: " << edit_station.workshop << endl;
 				cout << "Enter correct data: ";
-				cin >> edit_station.active_workshop;
+				InputInt(edit_station.active_workshop);
 			}
 			break;
 		case 2:
@@ -259,7 +257,7 @@ int main()
 
 	while (true)
 	{
-		cout << "Menu: " << endl;
+		cout << endl << "Menu: " << endl;
 		cout << "1. Add pipe" << endl;
 		cout << "2. Add station" << endl;
 		cout << "3. All object" << endl;
@@ -267,10 +265,11 @@ int main()
 		cout << "5. Edit station" << endl;
 		cout << "6. Save" << endl;
 		cout << "7. Download" << endl;
-		cout << "0. Exit" << endl;
+		cout << "0. Exit" << endl << endl;
 
-		int choice;
-		cin >> choice;
+		cout << "Your choice: ";
+		int choice = 0;
+		InputInt(choice);
 
 		switch (choice)
 		{
@@ -278,49 +277,49 @@ int main()
 		{
 			cout << "Add pipe" << endl;
 			pipe = AddPipe();
-			cin >> choice;
+			break;
 		}
 		case 2:
 		{
 			cout << "Add station" << endl;
 			station = AddStation();
-			cin >> choice;
+			break;
 		}
 		case 3:
 		{
 			cout << "All object" << endl;
 			ShowPipe(pipe);
 			ShowStation(station);
-			cin >> choice;
+			break;
 		}
 		case 4:
 		{
 			cout << "Edit pipe" << endl;
 			EditPipe(pipe);
-			cin >> choice;
+			break;
 		}
 		case 5:
 		{
 			cout << "Edit station" << endl;
 			EditStation(station);
-			cin >> choice;
+			break;
 		}
 		case 6:
 		{
 			cout << "Save";
-			cin >> choice;
+			break;
 		}
 		case 7:
 		{
 			cout << "Download";
-			cin >> choice;
+			break;
 		}
 		case 0:
 			return 0;
 			break;
 		default:
 			cout << "Error!!! Try again." << endl;
-			cin >> choice;
+			break;
 		}
 	}
 
